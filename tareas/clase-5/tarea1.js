349df4afef74ff4d39d144d8dc34e6294b72352a
SantiagotGimenez <55916705+SantiagotGimenez@users.noreply.github.com>
2021-01-28 09:55:48 -0300

    Update tarea1.js
    código retocado

--- tareas/clase-5/tarea1.js
+++ tareas/clase-5/tarea1.js
@@ -1,3 +1,4 @@
+
 document.querySelector('#calcular-salario-mensual').onclick = function(){
     const salarioAnual = Number(document.querySelector('#salario-anual').value);
     const salarioMensual = calcularSalarioMensual(salarioAnual);
